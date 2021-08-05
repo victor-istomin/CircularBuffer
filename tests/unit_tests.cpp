85ae2cfc9689ac8e8925e8e158743a1100fbd81c
Victor Istomin <victor-istomin@users.noreply.github.com>
2021-08-05 16:43:35 +0300

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -9,8 +9,6 @@ TEST_CASE("tesing circular data overwriting")
 
     CHECK(ints.size() == 0);
     
-    CHECK(false);
-
     int testArray[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     for(int i = 0; i < (int)std::size(testArray); ++i)
     {
